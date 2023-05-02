92f43f149be3999ccca1baf61eb210cd34ac05c6
A-Tanner <atanner5@ewu.edu>
2023-05-01 17:00:12 -0700

    Add InputField component

---
+++ EagleTracks/components/InputField.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import { TextInput, View, Text } from 'react-native';
+
+const InputField = (props) => {
+    const [value, setValue] = useState(); //This is an integer
+    this.label = props.label;
+    this.checkFunctions = props.checkFunctions;
+    this.children=props.children //Assumed to be the barcode scanner or other component
+
+     function verifyData(){ 
+         //Note that checkFunctions is an array of functions
+       if (checkFunctions){
+         checkFunctions.forEach( fn => fn() );
+       }
+     }
+
+    return (
+        <View>
+        <Text style={props.style.textStyle}>{label}</Text>
+        <TextInput 
+            style = {props.style.input}
+            placeholder = {props.placeholder}
+            onChangeText={
+                    (entry)=>{
+                        setValue(entry)
+                        verifyData();
+                    }
+             }
+         >
+         </TextInput>
+            {children} 
+        </View>
+    )
+
+}
+
+export default InputField
